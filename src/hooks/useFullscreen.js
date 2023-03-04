d0b67ef44966fbd48cc7b3127aab10058c02ee0e
pourquoi21 <miyaku219@naver.com>
2023-03-05 02:51:18 +0900

    useFullscreen

---
+++ src/hooks/useFullscreen.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import "./styles.css";
+
+export default function App() {
+  const useFullscreen = (callback) => {
+    const element = useRef();
+    const triggerFull = () => {
+      if (element.current) {
+        element.current.requestFullscreen();
+        if (callback && typeof callback === "function") callback(true);
+      }
+    };
+    const exitFull = () => {
+      if (document.fullscreenElement !== null) document.exitFullscreen();
+      if (callback && typeof callback === "function") callback(false);
+    };
+    return { element, triggerFull, exitFull };
+  };
+
+  const onFullS = (isFull) => {
+    console.log(isFull ? "we are full" : "we are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+
+  return (
+    <div className="App">
+      <div ref={element}>
+        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
+    </div>
+  );
+}
