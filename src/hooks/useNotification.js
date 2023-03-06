c1e91ea6bbe7e7a3148db3e31d15c0ab1f79d837
pourquoi21 <miyaku219@naver.com>
2023-03-06 23:59:49 +0900

    commit

---
+++ src/hooks/useNotification.js
@@ -0,0 +1,27 @@
+export default function App() {
+  const useNotification = (title, option) => {
+    if (!("Notification" in window)) return;
+    const fireNotif = () => {
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            new Notification(title, option);
+          } else {
+            return;
+          }
+        });
+      } else {
+        new Notification(title, option);
+      }
+    };
+    return fireNotif;
+  };
+
+  const triggerNotif = useNotification("Can I?");
+
+  return (
+    <div className="App">
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+}
