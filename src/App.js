7dcd26d2c7bd62cfd7231556e33f09ce2eaef762
pourquoi21 <miyaku219@naver.com>
2023-03-08 23:53:01 +0900

    preparing for useAxios...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+import "./styles.css";
+import useAxios from "./useAxios";
+
+export default function App() {
+  const { loading, data, error } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+
+  console.log(loading, data, error);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
-
-export default App;
